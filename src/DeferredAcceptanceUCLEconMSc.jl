c8e281e966b5912dd7876cccfc7d6c8e5c3f209c
joaovgranja <joaov.granja@gmail.com>
2025-02-18 13:28:32 +0000

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.53

---
+++ src/DeferredAcceptanceUCLEconMSc.jl
@@ -0,0 +1,5 @@
+module DeferredAcceptanceUCLEconMSc
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using DeferredAcceptanceUCLEconMSc
+using Test
+
+@testset "DeferredAcceptanceUCLEconMSc.jl" begin
+    # Write your tests here.
+end
